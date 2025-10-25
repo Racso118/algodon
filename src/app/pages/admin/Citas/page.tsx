7ded8a7229adb8ffda7198666862c5b3c95829b9
Osca Ramos <oscar.16051@gmail.com>
2025-10-25 16:07:49 -0600

    Citas

--- src/app/pages/admin/Citas/page.tsx
+++ src/app/pages/admin/Citas/page.tsx
@@ -210,6 +210,7 @@ export default function CitasPage() {
                     onChange={(e) => setFiltro({ ...filtro, estado: e.target.value })}
                     className="w-full p-2 mb-2 border-2 border-gray-300 rounded-md focus:outline-none focus:border-blue-500 bg-white"
                   >
+                  
                     <option value="">Todos</option>
                     <option value="Pendiente">Pendiente</option>
                     <option value="Asignada">Asignada</option>
